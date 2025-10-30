686617714bc8916bf4c1b01f37b6bf2e999e4b39
muriki20 <mosesmuriki6113@gmail.com>
2025-10-30 22:26:05 +0300

    Add files via upload

---
+++ electricbill.c
@@ -0,0 +1,29 @@
+// c program 
+/*
+Name:moses
+reg:CT100/G/26258/25
+description:program withdraw and balance from bank 
+*/#include <stdio.h>
+
+float calculateElectricBill(int units) {
+    float bill = 0;
+
+    if (units <= 100)
+        bill = units * 10;
+    else if (units <= 200)
+        bill = (100 * 10) + (units - 100) * 15;
+    else
+        bill = (100 * 10) + (100 * 15) + (units - 200) * 20;
+
+    return bill;
+}
+
+int main() {
+    int units;
+    printf("Enter units consumed: ");
+    scanf("%d", &units);
+
+    printf("Total bill = KSh. %.2f\n", calculateElectricBill(units));
+    return 0;
+}
+    
