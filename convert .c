6574be4c1c3f4a3fec5bdbc941c747b555ee4576
muriki20 <mosesmuriki6113@gmail.com>
2025-10-30 22:38:59 +0300

    Add files via upload

---
+++ convert .c
@@ -0,0 +1,21 @@
+// c program 
+/*
+Name:moses
+reg:CT100/G/26258/25
+description:program withdraw and balance from bank 
+*/
+#include <stdio.h>
+
+float convertToCelsius(float fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+
+int main() {
+    float f;
+    printf("Enter temperature in Fahrenheit: ");
+    scanf("%f", &f);
+
+    printf("Temperature in Celsius = %.2f°C\n", convertToCelsius(f));
+    return 0;
+}
+    
